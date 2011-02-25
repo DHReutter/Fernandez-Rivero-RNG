cabcbd702fcd767c6295e7d9b9b71f4c68a2be2a
daniel <daniel@20cc7bd3-dd24-0410-b7d5-e471d65ade14>
2011-02-25 14:35:12 +0000

    Renamed test.c to nrandgen.c. nrandgen [n] [seed] [sd] writes a given number n of normally distributed random numbers with standard deviation sd and the given seed to stdout.
    git-svn-id: svn+ssh://141.84.226.140/svn/svnMischa/r/analysis/trunk/simulations/RDMs/Decision chain/fr_nrand@4412 20cc7bd3-dd24-0410-b7d5-e471d65ade14

--- nrandgen.c
+++ nrandgen.c
@@ -1,6 +1,6 @@
 /*
  * Fernandez-Rivero-RNG - A fast normal distribution pseudo-random number generator
- * Copyright (C) 2009 by Daniel Reutter <reutter@psy.lmu.de>
+ * Copyright (C) 2009-2010 by Daniel Reutter <reutter@psy.lmu.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,22 +19,10 @@
 #include "nrand.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
-
-inline double sqr(double x) {
-	return x * x;
-}
 
 int main(int argc, char *argv[]) {
 
-	double rn = 0;
-
-	double A = 0, Q = 0;
-
-	clock_t c0, c1;
-
-	double sd, calcsd;
+	double sd;
 	int n, seed;
 	int i;
 
@@ -49,27 +37,9 @@ int main(int argc, char *argv[]) {
 
 	snrand(seed, sd);
 
-	c0 = clock();
-
 	for (i = 0; i < n; ++i) {
-		rn = 5 + nrand();
-		if (i) {
-			Q = Q + (i * sqr(rn - A)) / (i+1);
-			A = A + (rn - A)/(i+1);
-		} else {
-			A = rn;
-			Q = 0;
-		}
+		printf("%f\n", nrand());
 	}
-
-	c1 = clock();
-
-	printf("Last number was %f\n", rn);
-	printf("Mean is %f\n", A);
-	calcsd = sqrt(Q / n);
-	printf("Standard deviation is %f (deviates %f from %f)\n", calcsd, sd - calcsd, sd);
-
-	printf("\nThe calculation took %d clock cycles (%.3f milliseconds; %.3f microsec/call).\n", (int)(c1-c0), (c1-c0)/(double)CLOCKS_PER_SEC*1000, (c1-c0)/(double)CLOCKS_PER_SEC*1000000/n);
 
 	return 0;
 }
